13b3067d39c4b30412c6b688927e20fca7bfc841
Termo <termopeten@gmail.com>
2021-03-01 22:24:43 +0100

    re-factor

--- resources/lib/addon.py
+++ resources/lib/addon.py
@@ -32,22 +32,35 @@ import xbmc
 import xbmcgui
 import xbmcplugin
 import xbmcaddon
-
-try:
-    import json
-except:
-    import simplejson as json
+import json
 
 BASE_URL = 'http://www.dr.dk/bonanza/'
+tr = xbmcaddon.Addon().getLocalizedString
+addon_path = xbmcaddon.Addon().getAddonInfo('path')
+addon_name = xbmcaddon.Addon().getAddonInfo('name')
+ICON = os.path.join(addon_path, 'resources', 'icon.png')
+FANART = os.path.join(addon_path, 'resources', 'fanart.jpg')
+CACHE = os.path.join(addon_path, 'requests_cache')
 
+def make_notice(object):
+    xbmc.log(str(object), xbmc.LOGNOTICE )
 
 class BonanzaException(Exception):
     pass
 
 
 class Bonanza(object):
+    def __init__(self, plugin_url, plugin_handle):
+        self._plugin_url = plugin_url
+        self._plugin_handle = plugin_handle
+
+        #cache expires after: 86400=1 day   604800=7 days
+        requests_cache.install_cache(CACHE, backend='sqlite', expire_after=86400 )
+        requests_cache.remove_expired_responses()
+
     def search(self):
-        keyboard = xbmc.Keyboard('', ADDON.getLocalizedString(30001))
+        make_notice('search')
+        keyboard = xbmc.Keyboard('', tr(30001))
         keyboard.doModal()
         if keyboard.isConfirmed():
             html = self._downloadUrl('http://www.dr.dk/bonanza/sog?q=' + keyboard.getText().replace(' ', '+'))
@@ -66,55 +79,55 @@ class Bonanza(object):
                 infoLabels = {
                     'title': title,
                     'plot': description,
-                    'studio': ADDON.getAddonInfo('name')}
+                    'studio': addon_name}
 
-                item = xbmcgui.ListItem(infoLabels['title'], iconImage=image, thumbnailImage=image)
-                item.setProperty('Fanart_Image', FANART)
+                item = xbmcgui.ListItem(infoLabels['title'])
+                item.setArt({'fanart': FANART, 'icon': image, 'thumb': image})
                 item.setProperty('IsPlayable', 'true')
                 item.setInfo('video', infoLabels)
 
                 url = '?mode=play&url=' + url
-                items.append((PATH + url, item, False))
-            xbmcplugin.addDirectoryItems(HANDLE, items)
+                items.append((self._plugin_url + url, item, False))
+            xbmcplugin.addDirectoryItems(self._plugin_handle, items)
 
-            xbmcplugin.endOfDirectory(HANDLE)
+            xbmcplugin.endOfDirectory(self._plugin_handle)
 
     def showCategories(self):
         items = list()
         html = self._downloadUrl(BASE_URL)
 
-        item = xbmcgui.ListItem(ADDON.getLocalizedString(30001), iconImage=ICON)
-        item.setProperty('Fanart_Image', FANART)
-        xbmcplugin.addDirectoryItem(HANDLE, PATH + '?mode=search', item, True)
+        item = xbmcgui.ListItem(tr(30001))
+        item.setArt({'fanart': FANART, 'icon': ICON})
+        xbmcplugin.addDirectoryItem(self._plugin_handle, self._plugin_url + '?mode=search', item, True)
 
         pattern = '<a href="(/bonanza/kategori/.*)">(.*)</a>'
         for m in re.finditer(pattern, html):
             path = m.group(1)
             title = m.group(2)
 
-            item = xbmcgui.ListItem(title, iconImage=ICON)
-            item.setProperty('Fanart_Image', FANART)
+            item = xbmcgui.ListItem(title)
+            item.setArt({'fanart': FANART, 'icon': ICON})
             item.setInfo('video', infoLabels={
                 'title': title
             })
-            url = PATH + '?mode=subcat&url=http://www.dr.dk' + path
+            url = self._plugin_url + '?mode=subcat&url=http://www.dr.dk' + path
             items.append((url, item, True))
 
-        xbmcplugin.addDirectoryItems(HANDLE, items)
-        xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_TITLE)
-        xbmcplugin.endOfDirectory(HANDLE)
+        xbmcplugin.addDirectoryItems(self._plugin_handle, items)
+        xbmcplugin.addSortMethod(self._plugin_handle, xbmcplugin.SORT_METHOD_TITLE)
+        xbmcplugin.endOfDirectory(self._plugin_handle)
 
     def showSubCategories(self, url):
         html = self._downloadUrl(url.replace(' ', '+'))
         self.addSubCategories(html)
-        xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_TITLE)
-        xbmcplugin.endOfDirectory(HANDLE)
+        xbmcplugin.addSortMethod(self._plugin_handle, xbmcplugin.SORT_METHOD_TITLE)
+        xbmcplugin.endOfDirectory(self._plugin_handle)
 
     def showContent(self, url):
         html = self._downloadUrl(url)
         self.addContent(html)
 
-        xbmcplugin.endOfDirectory(HANDLE)
+        xbmcplugin.endOfDirectory(self._plugin_handle)
 
     def addSubCategories(self, html):
         pattern = '<a href="/bonanza/(serie/.*?)".*?' \
@@ -126,14 +139,14 @@ class Bonanza(object):
             title = self._decodeHtmlEntities(m.group(3))
             description = self._decodeHtmlEntities(m.group(4))
 
-            item = xbmcgui.ListItem(title, iconImage=image)
-            item.setProperty('Fanart_Image', FANART)
+            item = xbmcgui.ListItem(title)
+            item.setArt({'fanart': FANART, 'icon': image})
             item.setInfo('video', infoLabels={
                 'title': title,
                 'plot': description
             })
-            url = PATH + '?mode=content&url=' + url
-            xbmcplugin.addDirectoryItem(HANDLE, url, item, True)
+            url = self._plugin_url + '?mode=content&url=' + url
+            xbmcplugin.addDirectoryItem(self._plugin_handle, url, item, True)
 
     def addContent(self, html):
         items = list()
@@ -150,16 +163,16 @@ class Bonanza(object):
             infoLabels = {
                 'title': title,
                 'plot': description,
-                'studio': ADDON.getAddonInfo('name')}
+                'studio': addon_name}
 
-            item = xbmcgui.ListItem(infoLabels['title'], iconImage=image, thumbnailImage=image)
-            item.setProperty('Fanart_Image', FANART)
+            item = xbmcgui.ListItem(infoLabels['title'])
+            item.setArt({'fanart': FANART, 'icon': image, 'thumb': image})
             item.setProperty('IsPlayable', 'true')
             item.setInfo('video', infoLabels)
 
             url = '?mode=play&url=' + url
-            items.append((PATH + url, item, False))
-        xbmcplugin.addDirectoryItems(HANDLE, items)
+            items.append((self._plugin_url + url, item, False))
+        xbmcplugin.addDirectoryItems(self._plugin_handle, items)
 
 
     def playContent(self, url):
@@ -168,9 +181,9 @@ class Bonanza(object):
         m = re.search(pattern, html, re.DOTALL)
         if m is not None:
             item = xbmcgui.ListItem(path=m.group(1))
-            xbmcplugin.setResolvedUrl(HANDLE, True, item)
+            xbmcplugin.setResolvedUrl(self._plugin_handle, True, item)
         else:
-            xbmcplugin.setResolvedUrl(HANDLE, False, xbmcgui.ListItem())
+            xbmcplugin.setResolvedUrl(self._plugin_handle, False, xbmcgui.ListItem())
 
     def _downloadUrl(self, url):
         try:
@@ -219,10 +232,30 @@ class Bonanza(object):
 
     def showError(self, message):
         heading = buggalo.getRandomHeading()
-        line1 = ADDON.getLocalizedString(30900)
-        line2 = ADDON.getLocalizedString(30901)
+        line1 = tr(30900)
+        line2 = tr(30901)
         xbmcgui.Dialog().ok(heading, line1, line2, message)
 
+    def route(self, query):
+        try:
+            PARAMS = dict(urlparse.parse_qsl(query[1:]))
+            if 'mode' in PARAMS:
+                if PARAMS['mode'] == 'subcat':
+                    self.showSubCategories(PARAMS['url'])
+                elif PARAMS['mode'] == 'content':
+                    self.showContent(PARAMS['url'])
+                elif PARAMS['mode'] == 'search':
+                    self.search()
+                elif PARAMS['mode'] == 'play':
+                    self.playContent(PARAMS['url'])
+            else:
+                self.showCategories()
+
+        except BonanzaException as ex:
+            self.showError(str(ex))
+
+        except Exception:
+            buggalo.onExceptionRaised()
 
 if __name__ == '__main__':
     ADDON = xbmcaddon.Addon()
